8f08a4a6deca56fff1f6e3d89230d7cf1b552789
Henrique <henriquevenancio99@outlook.com>
2018-11-29 22:48:00 -0200

    Implementando funcoes com arquivos

--- projMenuBarList/src/pct/Principal.java
+++ projMenuBarList/src/pct/Principal.java
@@ -7,12 +7,18 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.spi.FileTypeDetector;
+import java.util.ArrayList;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -28,11 +34,13 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.filechooser.FileFilter;
 
 public class Principal implements ListSelectionListener, ActionListener {
 
@@ -48,10 +56,17 @@ public class Principal implements ListSelectionListener, ActionListener {
     JMenu menu, submenu;
     JMenuItem open, save, quit;
 
+    DefaultListModel<String> listModel;
+
     JFileChooser chooser;
     FileInputStream fis;
     BufferedReader br;
+    BufferedWriter bw;
     //static Principal app;
+
+    Publicacao nota;
+
+    ArrayList<Publicacao> anotacoes = new ArrayList<Publicacao>();
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
@@ -66,13 +81,13 @@ public class Principal implements ListSelectionListener, ActionListener {
         JPanel tudo = new JPanel(new BorderLayout());
         JFrame frame = new JFrame("JList");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 420);
+        frame.setSize(600, 428);
         frame.setVisible(true);
         frame.setContentPane(tudo);
 
         //MENU        
         menuBar = new JMenuBar();
-        menu = new JMenu("A Menu");
+        menu = new JMenu("ARQUIVO");
         menuBar.add(menu);
 
         open = new JMenuItem("OPEN");
@@ -92,9 +107,8 @@ public class Principal implements ListSelectionListener, ActionListener {
         tudo.add(menuBar, BorderLayout.NORTH);
 
         //LEFT
-        DefaultListModel<String> listModel = new DefaultListModel<String>();
-        //listModel.addElement("Figura 1");
-        //listModel.addElement("Figura 2");
+        listModel = new DefaultListModel<String>();
+
         list = new JList<String>(listModel);
         list.setPreferredSize(new Dimension(200, 100));
         list.addListSelectionListener(this);
@@ -107,10 +121,10 @@ public class Principal implements ListSelectionListener, ActionListener {
         JPanel panelTitulo = new JPanel(new GridLayout(3, 1));
         JPanel panelDescricao = new JPanel(new GridLayout(2, 1));
 
-        lblTitulo = new JLabel("T�tulo:");
+        lblTitulo = new JLabel("Título:");
         txtTitulo = new JTextField(25);
-        lblDescricao = new JLabel("Descri��o:");
-        txtArea = new JTextArea(15, 24);
+        lblDescricao = new JLabel("Descrição:");
+        txtArea = new JTextArea(16, 24);
         JScrollPane panelTextA = new JScrollPane(txtArea);
         panelTextA.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
@@ -121,8 +135,16 @@ public class Principal implements ListSelectionListener, ActionListener {
 
         JPanel panelButtons = new JPanel();
         btnAdd = new JButton("Adicionar");
+        btnAdd.setActionCommand("Add");
+        btnAdd.addActionListener(this);
+
         btnDelete = new JButton("Remover");
+        btnDelete.setActionCommand("Delete");
+        btnDelete.addActionListener(this);
+
         btnEdit = new JButton("Alterar");
+        btnEdit.setActionCommand("Edit");
+        btnEdit.addActionListener(this);
 
         panelButtons.add(btnAdd);
         panelButtons.add(btnDelete);
@@ -134,36 +156,120 @@ public class Principal implements ListSelectionListener, ActionListener {
         panelRight.add(panelDescricao);
 
         tudo.add(panelRight, BorderLayout.CENTER);
+
     }
 
     @Override
     public void valueChanged(ListSelectionEvent e) {
-        // TODO Auto-generated method stub
-        if (list.getSelectedValue().equalsIgnoreCase("")) {
+        // TODO Auto-generated method stub        
+        int selectedIndex = list.getSelectedIndex();
 
+        if (selectedIndex != -1) {
+            Publicacao notaSelecionada = anotacoes.get(selectedIndex);
+            setNota(notaSelecionada);
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        chooser = new JFileChooser();
-        if (chooser.showOpenDialog(menu) == JFileChooser.APPROVE_OPTION) {
 
-            Path path = chooser.getSelectedFile().toPath();
+        if (e.getActionCommand().equalsIgnoreCase("Add")) {
 
-            try {
-                String contentString = "";
+            Publicacao nota = new Publicacao();
 
-                for (String s : Files.readAllLines(path, StandardCharsets.UTF_8)) {
-                    contentString += s;
+            nota.setTitulo(txtTitulo.getText());
+            nota.setDescricao(txtArea.getText());
+
+            anotacoes.add(nota);
+
+            listModel.addElement(nota.getTitulo());
+
+            txtArea.setText("");
+            txtTitulo.setText("");
+        }
+
+        if (e.getActionCommand().equalsIgnoreCase("Delete")) {
+
+            int selectedIndex = list.getSelectedIndex();
+
+            anotacoes.remove(selectedIndex);
+            listModel.remove(selectedIndex);
+
+            txtArea.setText("");
+            txtTitulo.setText("");
+        }
+
+        if (e.getActionCommand().equalsIgnoreCase("Edit")) {
+
+            int selectedIndex = list.getSelectedIndex();
+
+            Publicacao p = anotacoes.get(selectedIndex);
+
+            p.setTitulo(txtTitulo.getText());
+            p.setDescricao(txtArea.getText());
+
+            listModel.set(selectedIndex, p.getTitulo());
+
+            txtArea.setText("");
+            txtTitulo.setText("");
+        }
+
+        if (e.getSource() == open) {
+            chooser = new JFileChooser();
+            if (chooser.showOpenDialog(menu) == JFileChooser.APPROVE_OPTION) {
+
+                Path path = chooser.getSelectedFile().toPath();
+
+                try {
+                    FileReader reader = new FileReader(chooser.getSelectedFile());
+                    br = new BufferedReader(reader);
+
+                    String linhas = null;
+                    String texto = new String();
+                    while ((linhas = br.readLine()) != null) {
+                        texto += linhas;
+                    }
+
+                    br.close();
+
+                    int pos = texto.indexOf(";");
+                    String titulo = texto.substring(0, pos);
+                    String descricao = texto.substring(pos + 1, texto.length() - 1);
+
+                    txtTitulo.setText(titulo);
+                    txtArea.setText(descricao);
+
+                } catch (IOException ea) {
+                    // TODO Auto-generated catch block
+                    ea.printStackTrace();
                 }
-
-                txtArea.setText(contentString);
-
-            } catch (IOException ea) {
-                // TODO Auto-generated catch block
-                ea.printStackTrace();
             }
         }
+
+        if (e.getSource() == save) {
+            chooser = new JFileChooser();
+            if (chooser.showSaveDialog(menu) == JFileChooser.APPROVE_OPTION) {
+                String text = txtTitulo.getText() + ";" + txtArea.getText() + ";";
+                File file = chooser.getSelectedFile();
+
+                try {
+                    FileWriter writer = new FileWriter(file.getPath());
+                    bw = new BufferedWriter(writer);
+                    bw.write(text);
+                    bw.close();
+
+                } catch (Exception ae) {
+                }
+            }
+        }
+
+        if (e.getSource() == quit) {                     
+            if(JOptionPane.showConfirmDialog(null, "Deseja realmente sair?") == 0) System.exit(0);
+        }
+    }
+
+    public void setNota(Publicacao p) {
+        txtArea.setText(p.getDescricao());
+        txtTitulo.setText(p.getTitulo());
     }
 }
